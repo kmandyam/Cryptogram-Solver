d314a6a977f42e1e69ca32a3eb87d5a035bd22b4
kmandyam <Karishma@ho-mba-843.local>
2016-01-28 20:04:20 -0800

    Optimized function to prune dictionary by eliminating loops

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -4,12 +4,14 @@ public class Dictionary {
 
     private Set<String> dictionary;
     private Map<Integer,Set<String>> lengthToWords;
+    private Map<String, List<String>> patternToWords;
 
     //hashmap of past entries
 
     public Dictionary(List<String> dictionaryTokens){
         dictionary = new HashSet<String>();
         lengthToWords = new HashMap<Integer, Set<String>>();
+        patternToWords = new HashMap<String, List<String>>();
         for(String word: dictionaryTokens){
             dictionary.add(word);
             if(!(lengthToWords.keySet().contains(word.length()))){
@@ -23,43 +25,34 @@ public class Dictionary {
 
     }
 
-    public Set<String> pruneDictByPattern(String pattern){
-        Map<String, String> patternMap= new HashMap<String, String>();
-        List<String> prunedDict = new ArrayList<String>();
-        prunedDict.addAll(lengthToWords.get(pattern.length()));
-        List<String> prunePattern = convertWordToPattern(pattern, prunedDict);
+    public List<String> pruneDictByPattern(String pattern){
 
-        for(int i = 0; i < prunedDict.size(); i++){
-            patternMap.put(prunedDict.get(i), prunePattern.get(i));
-        }
-
-        List<String> removedWords = new ArrayList<String>();
-        for(String s: patternMap.keySet()){
-            if(!patternMap.get(s).equals(pattern)){
-                removedWords.add(s);
+        if(patternToWords.containsKey(pattern)){
+            return patternToWords.get(pattern);
+        }else {
+            Set<String> prunedDict = lengthToWords.get(pattern.length());
+            List<String> result = new ArrayList<String>();
+            for (String w : prunedDict) {
+                String wp = convertWordToPattern(pattern, w);
+                if (wp.equals(pattern)) {
+                    result.add(w);
+                }
             }
+            patternToWords.put(pattern, result);
+            return result;
         }
-        for(String s: removedWords){
-            patternMap.remove(s);
-        }
-
-        return patternMap.keySet();
     }
 
-    private List<String> convertWordToPattern(String pattern, List<String> prunedDict){
-        List<String> prunePattern = new ArrayList<String>();
-        for(String word : prunedDict){
-            String modifiedWord = "";
-            for(int i = 0; i < pattern.length(); i++){
-                char wordChar = word.charAt(i);
-                char patternChar = pattern.charAt(i);
-                if(patternChar == '-'){
-                    wordChar = '-';
-                }
-                modifiedWord += wordChar;
+    private String convertWordToPattern(String pattern, String word){
+        StringBuilder modifiedWord = new StringBuilder();
+        for (int i = 0; i < pattern.length(); i++) {
+            char wordChar = word.charAt(i);
+            char patternChar = pattern.charAt(i);
+            if (patternChar == '-') {
+                wordChar = '-';
             }
-            prunePattern.add(modifiedWord);
+            modifiedWord.append(wordChar);
         }
-        return prunePattern;
+        return modifiedWord.toString();
     }
 }
